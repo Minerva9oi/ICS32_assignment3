ca881e50951496585024b5999faf027a6879f463
Bozhang Zhou <bozhangz@uci.edu>
2026-05-22 15:44:25 -0700

    Implement DSP protocol helpers

--- ds_protocol.py
+++ ds_protocol.py
@@ -4,16 +4,50 @@
 
 # Replace the following placeholders with your information.
 
-# NAME
-# EMAIL
-# STUDENT ID
+# NAME Bozhang Zhou 
+# EMAIL bozhangz@uci.edu
+# STUDENT ID 93213406
 
+import time
 import json
 from collections import namedtuple
 
 # Namedtuple to hold the values retrieved from json messages.
 # TODO: update this named tuple to use DSP protocol keys
-DataTuple = namedtuple('DataTuple', ['foo','baz'])
+DataTuple = namedtuple('DataTuple', ['type','message','token'])
+
+def join_msg(username, password):
+  msg = {
+          "join":{
+            "username":username,
+            "password": password, 
+            "token":""
+            }
+        }
+  return json.dumps(msg)
+
+
+def post_msg(token, message):
+  msg = {
+          "token":token, 
+          "post": {
+                "entry":message, 
+                "timestamp":str(time.time())
+                  }
+          }
+  return json.dumps(msg)
+
+
+def bio_msg(token, bio):
+  msg = {
+        "token": token,
+        "bio": {
+          "entry": bio,
+          "timestamp": str(time.time())
+        }
+  }
+  return json.dumps(msg)
+
 
 def extract_json(json_msg:str) -> DataTuple:
   '''
@@ -23,9 +57,21 @@ def extract_json(json_msg:str) -> DataTuple:
   '''
   try:
     json_obj = json.loads(json_msg)
-    foo = json_obj['foo']
-    baz = json_obj['bar']['baz']
+    response = json.obj["response"]
+
+    response_type = response["type"]
+    message = response["message"]
+
+    if "token" in response:
+      token = response ["token"]
+    else:
+      token = ""
+    return DataTuple(response_type, message, token)
+  
   except json.JSONDecodeError:
     print("Json cannot be decoded.")
-
-  return DataTuple(foo, baz)
+    return DataTuple("error", "Json cannot be decoded.", "")
+  
+  except KeyError:
+    print("Invalid DSP response format")
+    return DataTuple("error", "Invalid DSP response format")
